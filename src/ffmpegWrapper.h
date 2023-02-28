20821cb4f62ec4b6fb9436ae5731ab13fc72d160
Haibo Chen <495810242@qq.com>
2023-02-28 14:44:06 +0800

    fix compile error

--- src/ffmpegWrapper.h
+++ src/ffmpegWrapper.h
@@ -35,6 +35,7 @@ av_ts_make_time_string((char*)_malloca(AV_TS_MAX_STRING_SIZE), ts, tb)
 av_make_error_string((char*)__builtin_alloca(AV_ERROR_MAX_STRING_SIZE), AV_ERROR_MAX_STRING_SIZE, errnum)
 #define av_ts2timestr(ts, tb) \
 av_ts_make_time_string((char*)__builtin_alloca(AV_TS_MAX_STRING_SIZE), ts, tb)
+#endif
 
 typedef std::function<int(void *user, uintptr_t handle, uint8_t *data, size_t length)> FF_RAW_DATA_CALLBACK;
 typedef std::function<int(void *user, uintptr_t handle, int err_code, const uint8_t *err_desc)> FF_EXCEPTION_CALLBACK;
